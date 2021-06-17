74bc8dd09ca36bac05d1d6ff3df647c621a7f874
Umutonipamera <umutonipamera@gmail.com>
2021-06-17 12:08:56 +0300

    pythonquiz

---
+++ python_test.py
@@ -0,0 +1,33 @@
+x=[100,110,120,130,140,150]
+a= [item * 5 for item in x]
+print(a)
+  
+x=[3,6,8,2,4,1,5,7]
+
+print(min(x))
+
+def divisible_by_three(n):
+  
+      for a in n(1<100):
+       if(a%3==0):
+           n+=a
+            
+      print(n)
+      
+divisible_by_three(1<100)
+
+def divisible_by_seven():
+	n=[]
+	for x in range(100, 200):
+	    if (x%7==0):
+	        n.append(str(x))
+	print (','.join(n))
+        
+divisible_by_seven()
+
+x=[[1,2],[3,4],[5,6]]
+
+z = []
+print(z.extend, ([1, 2],[3,4], [ 5, 6]))
+
+
